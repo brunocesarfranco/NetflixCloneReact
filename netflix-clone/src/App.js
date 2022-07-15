edded1598fd27ac025af73d221172a0854d95f50
Bruno Cesar Franco <brunocesarfranco@gmail.com>
2022-07-15 19:02:34 -0300

    Integration with API (TMDB)
    Const API_KEY;
    Const Categories;
    Method getMovies;

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      {/* Navbar */}
+      {/* Destaque */}
+      {/* Em Alta */}
+      {/* Filmes Categorias */}
     </div>
   );
 }
