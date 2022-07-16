1792036b5644defad213eb6572619bc22264975a
Bruno Cesar Franco <brunocesarfranco@gmail.com>
2022-07-16 14:44:35 -0300

    Add Nav, ReactPlayer + MovieTrailer
    npm i movie-trailer;
    npm i react-player;
    Add nav;

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,28 +1,26 @@
-import './App.css';
-import Row from './components/Row';
-import categories from './api';
-import Banner from './components/Banner';
+import "./App.css";
+import Row from "./components/Row";
+import categories from "./api";
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      {/* Navbar */}
-      {/* Destaque */}
-      <Banner/>
-      {/* Em Alta */}
+      <Nav />
+      <Banner />
       {categories.map((category) => {
         return (
           <Row
-            key={category.name} 
-            title={category.title} 
+            key={category.name}
+            title={category.title}
             path={category.path}
             isLarge={category.isLarge}
-        />
+          />
         );
       })}
     </div>
   );
 }
-
 
 export default App;
--- netflix-clone/src/components/Row.js
+++ netflix-clone/src/components/Row.js
@@ -1,18 +1,32 @@
-import React, { useEffect } from 'react';
-import { getMovies } from '../api';
-import './Row.css';
+import React, { useEffect } from "react";
+import ReactPlayer from "react-player";
+import movieTrailer from "movie-trailer";
+import { getMovies } from "../api";
+import "./Row.css";
 
-const imageHost = "http://image.tmdb.org/t/p/original/";
-
-function Row({title, path, isLarge}) {
+const imageHost = "https://image.tmdb.org/t/p/original/";
+function Row({ title, path, isLarge }) {
   const [movies, setMovies] = React.useState([]);
-  
+  const [trailerUrl, setTrailerUrl] = React.useState("");
+  const handleOnClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie.title || movie.name || movie.original_name || "")
+        .then((url) => {
+          setTrailerUrl(url);
+        })
+        .catch((error) => {
+          console.log("Error fetching movie trailer: ", error);
+        });
+    }
+  };
+
   const fetchMovies = async (_path) => {
     try {
-        const data = await getMovies(_path);
-        console.log("data", data);
-        setMovies(data?.results);
-
+      const data = await getMovies(_path);
+      console.log("data ", data);
+      setMovies(data?.results);
     } catch (error) {
       console.log("fetchMovies error: ", error);
     }
@@ -21,22 +35,26 @@ function Row({title, path, isLarge}) {
   useEffect(() => {
     fetchMovies(path);
   }, [path]);
-  
+
   return (
     <div className="row-container">
       <h2 className="row-header">{title}</h2>
       <div className="row-cards">
-        {movies?.map(movie => {
+        {movies?.map((movie) => {
           return (
-              <img 
+            <img
               className={`movie-card ${isLarge && "movie-card-large"}`}
-              key={movie.id} 
-              src={`${imageHost}${isLarge ? movie.poster_path : movie.poster_path}`} 
-              alt={movie.name}>            
-              </img>
-          )                
+              onClick={() => handleOnClick(movie)}
+              key={movie.id}
+              src={`${imageHost}${
+                isLarge ? movie.backdrop_path : movie.poster_path
+              }`}
+              alt={movie.name}
+            ></img>
+          );
         })}
       </div>
+      {trailerUrl && <ReactPlayer url={trailerUrl} playing={true} />}
     </div>
   );
 }
