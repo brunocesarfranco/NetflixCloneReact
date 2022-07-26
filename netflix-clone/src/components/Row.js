73ee2e1bba0e311b85eb935e9f805ee88c09714f
Elifio <91218388+ElifioJr@users.noreply.github.com>
2022-07-25 21:58:19 -0300

    NETFLIX-CLONE-001 / Foi implementado: biblioteca de trailers, firebase e deploy

--- netflix-clone/src/components/Row.js
+++ netflix-clone/src/components/Row.js
@@ -1,31 +1,32 @@
-import movieTrailer from "movie-trailer";
 import React, { useEffect } from "react";
 import ReactPlayer from "react-player";
+import movieTrailer from "movie-trailer";
 import { getMovies } from "../api";
 import "./Row.css";
 
 const imageHost = "https://image.tmdb.org/t/p/original/";
 function Row({title, path, isLarge}) {
   const [movies, setMovies] = React.useState([]);
-  const [trailerUrl, setTrailerUrl] = React.useState("")
+  const [trailerUrl, setTrailerUrl] = React.useState("");
   const handleOnClick = (movie) => {
     if(trailerUrl){
-      setTrailerUrl("")
-    }else{
+      setTrailerUrl("");
+    } else{
       movieTrailer(movie.title || movie.name || movie.original_name || "")
       .then((url) =>{
-        setTrailerUrl();
+        setTrailerUrl(url);
       })
     .catch((error)=> {
       console.log("Error fetching movie trailer: ", error)
-    })}};
+    });
+  }
+};
   
   const fetchMovies = async (_path) => {
     try {
       const data = await getMovies(_path);
       console.log("data", data);
       setMovies(data?.results);
-
     } catch (error) {
       console.log("fetchMovies error: ", error);
       }
@@ -39,11 +40,11 @@ function Row({title, path, isLarge}) {
     <div className="row-container">
       <h2 className="row-header">{title}</h2>
       <div className="row-cards">
-        {movies?.map(movie => {
+        {movies?.map((movie) => {
           return (
           <img 
             className={`movie-card ${isLarge && "movie-card-large"}`}
-            onClick={()=> handleOnClick(movie)}
+            onClick={() => handleOnClick(movie)}
             key={movie.id} 
             src={`${imageHost}${
               isLarge ? movie.backdrop_path : movie.poster_path
@@ -53,8 +54,9 @@ function Row({title, path, isLarge}) {
           );
         })}
       </div>
-      {trailerUrl && <ReactPlayer url={trailerUrl} playing={true}/>}
+       {trailerUrl && <ReactPlayer url={trailerUrl} playing={true} />}
     </div>
   );
 }
+
 export default Row;
